b81f87d8f58b2c3e9f54f0dc9dcde23002947527
AGB74 <AGB74@Adrian>
2022-04-28 17:59:33 +0200

    version 1

---
+++ src/galan1ejemplo1git/Galan1Ejemplo1Git.java
@@ -0,0 +1,10 @@
+
+package galan1ejemplo1git;
+
+public class Galan1Ejemplo1Git {
+
+    public static void main(String[] args) {
+        System.out.println("hola soy adrian version1");
+    }
+    
+}
