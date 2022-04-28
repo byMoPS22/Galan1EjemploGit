a1d588f8a6e9224ab2f1d362f713a188cbd62abc
damt125 <damt125@36-09.aglinformatica.es>
2022-04-28 19:18:55 +0200

    (Version Saúl 2.1)

--- src/galan1ejemplo1git/Galan1Ejemplo1Git.java
+++ src/galan1ejemplo1git/Galan1Ejemplo1Git.java
@@ -4,7 +4,6 @@ package galan1ejemplo1git;
 public class Galan1Ejemplo1Git {
 
     public static void main(String[] args) {
-        System.out.println("hola soy adrian version2");
+        System.out.println("Hola soy Saúl version2");
     }
-    
 }
